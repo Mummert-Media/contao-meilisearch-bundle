4793b258148761db9ed3dbf31efc030d4ec91342
Jürgen Mummert <juergen@MacBookPro.fritz.box>
2025-12-21 20:37:58 +0100

    Remove migration; rely on DCA only

--- src/Resources/contao/dca/tl_search.php
+++ src/Resources/contao/dca/tl_search.php
@@ -1,43 +1,36 @@
 <?php
 
-$GLOBALS['TL_DCA']['tl_meilisearch'] = [
-    'config' => [
-        'dataContainer' => 'Table',
-        'sql' => [
-            'keys' => [
-                'id'  => 'primary',
-                'pid' => 'unique',
-            ],
-        ],
-    ],
+declare(strict_types=1);
 
-    'fields' => [
-        'id' => [
-            'sql' => "int(10) unsigned NOT NULL auto_increment",
-        ],
-        'pid' => [
-            'sql' => "int(10) unsigned NOT NULL default 0",
-        ],
-        'source' => [
-            'sql' => "varchar(32) NOT NULL default ''",
-        ],
-        'priority' => [
-            'sql' => "int(1) NOT NULL default 2",
-        ],
-        'keywords' => [
-            'sql' => "varchar(255) NOT NULL default ''",
-        ],
-        'imagepath' => [
-            'sql' => "varchar(512) NOT NULL default ''",
-        ],
-        'startDate' => [
-            'sql' => "bigint(20) NOT NULL default 0",
-        ],
-        'checksum' => [
-            'sql' => "varchar(64) NOT NULL default ''",
-        ],
-        'tstamp' => [
-            'sql' => "int(10) unsigned NOT NULL default 0",
-        ],
-    ],
+$GLOBALS['TL_DCA']['tl_search']['fields']['keywords'] = [
+    'label'     => ['Keywords', 'Suchbegriffe für die Indexierung'],
+    'exclude'   => true,
+    'inputType' => 'text',
+    'eval'      => ['tl_class' => 'w50', 'maxlength' => 255],
+    'sql'       => "varchar(255) NOT NULL default ''",
+];
+
+$GLOBALS['TL_DCA']['tl_search']['fields']['priority'] = [
+    'label'     => ['Priorität', 'Priorität für die Suchergebnisse'],
+    'exclude'   => true,
+    'inputType' => 'select',
+    'options'   => [1, 2, 3],
+    'eval'      => ['tl_class' => 'w50'],
+    'sql'       => "int(1) NOT NULL default '2'",
+];
+
+$GLOBALS['TL_DCA']['tl_search']['fields']['imagepath'] = [
+    'label'     => ['Image Path', 'Speichert den Pfad des Bildes'],
+    'exclude'   => true,
+    'inputType' => 'text',
+    'eval'      => ['maxlength' => 512],
+    'sql'       => "varchar(512) NOT NULL default ''",
+];
+
+$GLOBALS['TL_DCA']['tl_search']['fields']['startDate'] = [
+    'label'     => ['Startdatum', 'Startdatum für die Suchergebnisse (Unix-Timestamp)'],
+    'exclude'   => true,
+    'inputType' => 'text',
+    'eval'      => ['tl_class' => 'w50', 'rgxp' => 'digit'],
+    'sql'       => "bigint(20) NOT NULL default '0'",
 ];
