796e48c83c42bfb33b7c66b17906fd2c88016480
Jürgen Mummert <juergen@MacBookPro.fritz.box>
2025-12-21 19:20:08 +0100

    Extend tl_search with keywords, priority, imagepath and startDate

---
+++ src/Resources/contao/dca/tl_search.php
@@ -0,0 +1,35 @@
+<?php
+
+$GLOBALS['TL_DCA']['tl_search']['fields']['keywords'] = [
+    'label'     => ['Keywords', 'Suchbegriffe für die Indexierung'],
+    'exclude'   => true,
+    'inputType' => 'text',
+    'eval'      => ['tl_class' => 'w50', 'maxlength' => 255],
+    'sql'       => "varchar(255) NOT NULL default ''"
+];
+
+$GLOBALS['TL_DCA']['tl_search']['fields']['priority'] = [
+    'label'     => ['Priorität', 'Priorität für die Suchergebnisse'],
+    'exclude'   => true,
+    'inputType' => 'select',
+    'options'   => [1, 2, 3],
+    'default'   => 2,
+    'eval'      => ['tl_class' => 'w50'],
+    'sql'       => "int(1) NOT NULL default '2'"
+];
+
+$GLOBALS['TL_DCA']['tl_search']['fields']['imagepath'] = [
+    'label'     => ['Image Path', 'Speichert den Pfad des Bildes'],
+    'exclude'   => true,
+    'inputType' => 'text',
+    'eval'      => ['maxlength' => 512],
+    'sql'       => "varchar(512) NOT NULL default ''"
+];
+
+$GLOBALS['TL_DCA']['tl_search']['fields']['startDate'] = [
+    'label'     => ['Startdatum', 'Startdatum für die Suchergebnisse (Unix-Timestamp)'],
+    'exclude'   => true,
+    'inputType' => 'text',
+    'eval'      => ['rgxp' => 'digit', 'tl_class' => 'w50'],
+    'sql'       => "bigint(20) NOT NULL default '0'"
+];
