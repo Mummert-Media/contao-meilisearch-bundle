0ada038d21fddae6ccae438f0b58a4935e936547
Jürgen Mummert <juergen@MacBookPro.fritz.box>
2025-12-22 16:42:06 +0100

    Add Index Listener

--- src/EventListener/IndexPageListener.php
+++ src/EventListener/IndexPageListener.php
@@ -12,6 +12,12 @@ class IndexPageListener
 
     public function __invoke(string $content, array &$data, array &$set): void
     {
+
+        file_put_contents(
+            TL_ROOT . '/var/logs/meili-debug.log',
+            json_encode($set, JSON_PRETTY_PRINT) . "\n\n",
+            FILE_APPEND
+        );
         $searchData = $this->dataProvider->getSearchData($set);
 
         if ($searchData === null) {
