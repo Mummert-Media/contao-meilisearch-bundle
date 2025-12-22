1ba82dfb505324768cfd392e8b5a486a5dda2feb
Jürgen Mummert <juergen@MacBookPro.fritz.box>
2025-12-22 15:31:38 +0100

    BundlePluginInterface

--- src/ContaoManager/Plugin.php
+++ src/ContaoManager/Plugin.php
@@ -6,14 +6,21 @@ use Contao\CoreBundle\ContaoCoreBundle;
 use Contao\ManagerPlugin\Bundle\BundlePluginInterface;
 use Contao\ManagerPlugin\Bundle\Config\BundleConfig;
 use Contao\ManagerPlugin\Bundle\Parser\ParserInterface;
+use Contao\CalendarBundle\ContaoCalendarBundle;
+use Contao\NewsBundle\ContaoNewsBundle;
+use MummertMedia\ContaoMeilisearchBundle\MummertMediaContaoMeilisearchBundle;
 
 class Plugin implements BundlePluginInterface
 {
     public function getBundles(ParserInterface $parser)
     {
         return [
-            BundleConfig::create(\MummertMedia\ContaoMeilisearchBundle\ContaoMeilisearchBundle::class)
-                ->setLoadAfter([ContaoCoreBundle::class]),
+            BundleConfig::create(MummertMediaContaoMeilisearchBundle::class)
+                ->setLoadAfter([
+                    ContaoCoreBundle::class,
+                    ContaoCalendarBundle::class,
+                    ContaoNewsBundle::class,
+                ]),
         ];
     }
 }
