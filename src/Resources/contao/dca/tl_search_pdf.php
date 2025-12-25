ebbfdface7ab1eb5cc125b2d9f2974e4a4063c25
Jürgen Mummert <juergen@MacBookPro.fritz.box>
2025-12-25 14:15:02 +0100

    Bugfix

--- src/Resources/contao/dca/tl_search_pdf.php
+++ src/Resources/contao/dca/tl_search_pdf.php
@@ -10,6 +10,7 @@ $GLOBALS['TL_DCA']['tl_search_pdf'] = [
                 'id'       => 'primary',
                 'checksum' => 'unique',
                 'page_id'  => 'index',
+                'url'      => 'index',
             ],
         ],
     ],
@@ -23,23 +24,49 @@ $GLOBALS['TL_DCA']['tl_search_pdf'] = [
             'sql' => "int(10) unsigned NOT NULL default 0",
         ],
 
+        /*
+         * Absolute oder normalisierte PDF-URL
+         * z. B. /files/pdf/foo.pdf
+         */
         'url' => [
             'sql' => "varchar(1024) NOT NULL default ''",
         ],
 
+        /*
+         * Linktext oder Dateiname
+         */
         'title' => [
             'sql' => "varchar(255) NOT NULL default ''",
         ],
 
+        /*
+         * Geparster PDF-Text
+         */
         'text' => [
             'sql' => "mediumtext NULL",
         ],
 
+        /*
+         * md5(url + filemtime)
+         * → erkennt Änderungen zuverlässig
+         */
         'checksum' => [
             'sql' => "char(32) NOT NULL default ''",
         ],
 
+        /*
+         * Herkunftsseite (tl_page.id)
+         * → Cleanup / Referenz
+         */
         'page_id' => [
+            'sql' => "int(10) unsigned NOT NULL default 0",
+        ],
+
+        /*
+         * Dateizeitstempel der PDF
+         * → optional, aber extrem hilfreich
+         */
+        'file_mtime' => [
             'sql' => "int(10) unsigned NOT NULL default 0",
         ],
     ],
