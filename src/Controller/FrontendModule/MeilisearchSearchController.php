3041ff59b5224a0cd5e4bd1457f7fdea214de54f
Jürgen Mummert <juergen@MacBookPro.fritz.box>
2025-12-27 18:02:19 +0100

    Bugfix

--- src/Controller/FrontendModule/MeilisearchSearchController.php
+++ src/Controller/FrontendModule/MeilisearchSearchController.php
@@ -5,10 +5,12 @@ namespace MummertMedia\ContaoMeilisearchBundle\Controller\FrontendModule;
 use Contao\Config;
 use Contao\CoreBundle\Controller\FrontendModule\AbstractFrontendModuleController;
 use Contao\CoreBundle\Twig\FragmentTemplate;
+use Contao\CoreBundle\ServiceAnnotation\Asset;
 use Contao\ModuleModel;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
+#[Asset('css/meilisearch.css')]
 class MeilisearchSearchController extends AbstractFrontendModuleController
 {
     protected function getResponse(
@@ -18,7 +20,7 @@ class MeilisearchSearchController extends AbstractFrontendModuleController
     ): Response {
         $template->set('meiliLimit', (int) ($model->meiliLimit ?: 50));
 
-        // ✅ Config hier lesen
+        // Konfiguration sauber im Controller lesen
         $template->set('meiliHost', Config::get('meilisearch_host'));
         $template->set('meiliIndex', Config::get('meilisearch_index'));
         $template->set('meiliSearchKey', Config::get('meilisearch_api_search'));
